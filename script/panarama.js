ab1ba8a4b445ff64d8f3ac65b9b8d4c532c205c9
GENSECvkpb <gensec@users.noreply.github.com>
2021-05-21 16:51:20 +0300

    ver2.4

--- script/panarama.js
+++ script/panarama.js
@@ -1,13 +1,19 @@
 let panaram = document.querySelector(".panaram");
 let panaram_img = document.querySelector(".panaram_image");
 
+let new_panaram_img_Width;
+let new_panaram_img_Height;
+
 let screenWidth = window.innerWidth;
 let x;
+
+let label1;
+let label2;
 
 window.onload = funonload;
 
 function funonload() {
-  let panaram_img = document.querySelector(".panaram_image");
+  panaram_img = document.querySelector(".panaram_image");
   let panaram_img_Width = panaram_img.clientWidth;
 
   panaram.scrollTo(panaram_img_Width / 2,0);
@@ -32,21 +38,69 @@ panaram_img.addEventListener('mousemove', function(e) {
 
 function l_image (a) {
   document.panarama_img.src = a;
+      
+  label1 = document.getElementById("panaram__label-1");
+  label2 = document.getElementById("panaram__label-2");
 
-  let label1 = document.getElementById("panaram__label-1");
+  hideAllLabel();
+
+  setTimeout(() => {  
+    new_panaram_img = document.getElementById("panaram_image");
+    new_panaram_img_Width = new_panaram_img.clientWidth;
+    new_panaram_img_Height = new_panaram_img.clientHeight;
+
   
-  switch (a)
-  {
-    case "img/panaram/105.jpg": 
+    console.log(new_panaram_img_Width);
+    console.log(new_panaram_img_Height);
+  
+    switch (a)
     {
-      label1.style.left = "100px";
-      break;
+      case "img/panaram/105.jpg": 
+      {
+        label1.style.top = "320px";
+        label1.style.display = "block";
+        break;
+      }
+      case "img/panaram/methodical.jpg": 
+      {
+        label1.style.top = new_panaram_img_Height / 2.5 + "px";
+        label1.style.left = new_panaram_img_Width * 0.415 + "px";
+  
+        label1.style.display = "block";
+        break;
+      }
+      case "img/panaram/library.jpg": 
+      {
+        label1.style.top = new_panaram_img_Height / 2.5 + "px";
+        label1.style.left = new_panaram_img_Width * 0.8 + "px";
+        label1.style.display = "block";
+  
+        label2.style.top = "300px";
+        label2.style.left = "2800px";
+        label2.href = "javascript:l_image('img/panaram/library_2.jpg')"
+        label2.style.display = "block";
+        break;
+      }
+      case "img/panaram/library_2.jpg": 
+      {
+        label1.style.top = "320px";
+        label1.style.left = "420px";
+        label1.href = "javascript:l_image('img/panaram/library.jpg')"
+        label1.style.display = "block";
+        break;
+      }
+      case "img/panaram/212.jpg": 
+      {
+        label1.style.left = "500px";
+        break;
+      }
     }
-    case "img/panaram/212.jpg": 
-    {
-      label1.style.left = "500px";
-      break;
-    }
-  }
+
+  }, 1000);
+}
+
+function hideAllLabel(){
+  label1.style.display = "none";
+  label2.style.display = "none";
 }
 
